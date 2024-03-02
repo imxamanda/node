e7e36823c0589ce3c91b28048c293c5c7e84719a
imxamanda <amandavieiraperes.12@gmail.com>
2024-03-01 21:55:17 -0300

    aula 01/03

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Abriu o arquivo');
+const express = require('express');
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Rota principal')
+})
+
+app.get('/hello', function(req, res){
+    res.send('Chegou a resposta')
+})
+
+app.get('/nome', function(req, res){
+    res.send('Amanda Vieira Peres')
+})
+
+//console.log('Executou')
+
+app.listen(3000, function(){
+    console.log('Server up port 3000')
+})
