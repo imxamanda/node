ed606b2f9a976e7160b31fdbdcc515e1d350b1bc
Amanda Vieira <amandavieiraperes.12@gmail.com>
2024-03-13 08:44:59 -0300

    lista 1

--- routes/lista1.js
+++ routes/lista1.js
@@ -3,10 +3,13 @@ const  router = express.Router();
 
 router.post('/ex1', function(req, res){
 
-    const n1 = Number(req.body.n1)
+  /*   const n1 = Number(req.body.n1)
     const n2 = Number(req.body.n2)
     const n3 = Number(req.body.n3)
     const n4 = Number(req.body.n4)
+ */
+
+    const {n1, n2, n3, n4} = req.body
 
     const media = (n1 + n2 + n3 + n4) / 4
 
@@ -53,8 +56,110 @@ router.post('/ex2', function(req, res){
 
     retorno = {percBranco, percNulos, percValidos}
 
-    res.status(200).json(retorno)
+    res.json(retorno)
 }  
+})
+
+router.post('/ex3', function(req, res){
+    
+    const salAtual = Number(req.body.salario);
+
+        const reajuste = 0.09; 
+        const salNovo = salAtual * (1 + reajuste); 
+
+        retorno = {
+            codigo: salNovo,
+            mensagem: "Salário com reajuste!!" 
+        }
+
+        res.status(200).json(retorno)  
+})
+
+router.post('/ex4', function(req, res){
+
+    const cFabrica = Number(req.body.cFabrica)
+    const cDistribuidor =  cFabrica * 28/100
+    const cImposto = cFabrica * 45/100
+    const cFinal = cFabrica + cDistribuidor + cImposto
+
+    const retorno = {
+        Custo: cFinal
+    }
+        
+    res.json(retorno)         
+})
+
+router.post('/ex5', function(req, res){
+
+    const {cFabrica, percDistribuidor, percImposto} = req.body
+    const cDistribuidor =  cFabrica * Number(percDistribuidor/100)
+    const cImposto = cFabrica * (Number(percImposto) /100)
+    const cFinal = cFabrica + cDistribuidor + cImposto
+
+    const retorno = {
+        Custo: cFinal
+    }
+        
+    res.json(retorno)   
+})
+
+router.post('/ex6', function(req, res){
+
+    const {totalCarros, valCarro, valendas, salario} = req.body
+
+    const pagCarro = Number(totalCarros) * Number(valCarro)
+    const pagVenda = (Number(valendas) * 5 /100)
+    const salFinal = Number(salario) + pagCarro + pagVenda
+
+    const retorno = {
+        SalFinal: salFinal
+    }
+        
+    res.json(retorno) 
+
+})
+
+router.post('/ex7', function(req, res){
+
+    const n1 = Number(req.body.n1)
+    const n2 = Number(req.body.n2)
+
+    const soma = (n1 * 0.4) + (n2 * 0.6)
+    const media = soma / (0.4 + 0.6)
+
+    const retorno = {
+        mediaFinal: media
+    }
+
+    res.json(retorno)
+})
+
+router.post('/ex8', function(req, res){
+
+    const {raio, altura} = req.body
+    const pi = 3.14
+    const volume = pi * raio ** 2 * altura
+
+    const retorno = {
+        Volume: volume 
+    }
+
+   res.json(retorno)
+
+})
+
+router.post('/ex9', function(req, res){
+
+    const {num1, num2} = req.body
+
+    const soma = Number(num1) + Number(num2)
+    const multiplicado = soma * num1
+
+    const retorno = {
+        Multiplicado: multiplicado 
+    }
+
+   res.json(retorno)
     
 })
 
--- routes/lista2.js
+++ routes/lista2.js
@@ -1,21 +1,8 @@
 const express = require('express');
 const  router = express.Router();
 
-router.post("/ex1" ,(req,res)=>{
-    let n1 = Number(req.body.n1);
-    let n2 = Number(req.body.n2);
-    let n3 = Number(req.body.n3);
-    let n4 = Number(req.body.n4);
-
-    const media = (n1 + n2 + n3 + n4) / 4;
-    let retorno = {};
-    if((n1 > 10) || (n2 > 10) || (n3 > 10) ||( n4 > 10)){
-        res.status(400).json({erro : "Erro! A nota não deve passar de 10"})
-    }
-    else  {
-        retorno.msg = media >= 7 ? "Aprovado" : "Reprovado"
-        res.status(200).json(retorno);
-    }
+router.post("/ex1" , function(req,res){
+    
 })
 
 router.post('/ex3', function(req, res){
